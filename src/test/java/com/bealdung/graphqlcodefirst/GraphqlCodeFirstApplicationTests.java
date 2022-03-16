0d59a3bb5d805c684f716f0d1aaebbac481e0f02
suhit_kalubarme <suhit.k001@gmail.com>
2022-03-17 03:49:52 +0530

    initial commit

---
+++ src/test/java/com/bealdung/graphqlcodefirst/GraphqlCodeFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bealdung.graphqlcodefirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlCodeFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
